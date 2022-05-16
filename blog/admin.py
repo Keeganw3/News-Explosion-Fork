bb6b1aa37aef78b3a748427fa0f6466cd0e45fd6
Keeganw3 <keeganw749@gmail.com>
2022-05-16 15:29:47 +0000

    Add ability for admins to manage comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,8 +7,15 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'content')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
     list_filter = ('status', 'created_on')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    search_fields = ('name', 'email', 'created_on')
+    list_filter = ('approved', 'created_on')
