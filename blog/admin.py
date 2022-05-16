b957bede67f14c26c14459b48f3812b992c57fd1
Keeganw3 <keeganw749@gmail.com>
2022-05-16 15:14:19 +0000

    Posts display more information to admins such as slug and date posted

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
