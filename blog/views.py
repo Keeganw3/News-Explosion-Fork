eedbb4ed83f42c103866e2f543d74407b5243082
Keeganw3 <keeganw749@gmail.com>
2022-05-27 16:23:09 +0000

    [ADD] Comment functionality added for accounts

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,33 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(),
+                "left_comment": False
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        
+        make_comment = CommentForm(data=request.POST)
+
+        if make_comment.is_valid():
+            make_comment.instance.email = request.user.email
+            make_comment.instance.name = request.user.username
+            comment = make_comment.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            make_comment = CommentForm()
+        
+        return render(
+            request, "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "comment_form": CommentForm(),
+                "left_comment": True
             },
         )
