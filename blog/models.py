29a6a07bcbf7f5cadb1001a8ed07a1aa5933a2b6
Keeganw3 <keeganw749@gmail.com>
2022-06-29 23:55:05 +0000

    [UPDATE] Updated models to be more user friendly and made excerpt smandatory in post creation

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,18 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, unique=True, blank=False, null=False)
-    slug = models.SlugField(max_length=100, unique=True, blank=False, null=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    title = models.CharField(max_length=100, unique=True, 
+    blank=False, null=False)
+    slug = models.SlugField(max_length=100, unique=True, 
+    blank=False, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+    related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField(max_length=500, blank=False, null=False)
+    content = models.TextField(max_length=500, blank=False, 
+    null=False, default='Share your story!')
     featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(blank=True)
+    excerpt = models.TextField(max_length=100, blank=False, 
+    null=False, default='Tell us a little bit about your post!')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
@@ -27,10 +32,12 @@ class Post(models.Model):
         return self.likes.count()
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, 
+    related_name="comments")
+    name = models.CharField(max_length=100, default=False)
     email = models.EmailField()
-    body = models.TextField(max_length=200, blank=False, null=False)
+    body = models.TextField(default='Share your opinion!', max_length=500, 
+    blank=False, null=False)
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
@@ -39,3 +46,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
