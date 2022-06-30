98fc9e84c8962a996fe1f8a2a3adbec8700a6f4d
Keeganw3 <keeganw749@gmail.com>
2022-06-30 12:09:38 +0000

    Debug set to false

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True, 
     blank=False, null=False)
-    slug = models.SlugField(max_length=100, unique=True, 
-    blank=False, null=False)
+    slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, 
     related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
